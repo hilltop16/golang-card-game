0446cda39bcb70c40bf46a641cbd6975ef9d8b9c
Ben Lu <hilltop16@gmail.com>
2018-01-11 18:02:15 +0800

    added open file function

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+	"os"
+)
 
 type deck []string
 
@@ -30,4 +35,22 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck){
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(5)
+	}
+	s := deck(strings.Split(string(bs), ","))
+	return s
 }
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
+import "fmt"
 
 func main() {
 	//cards := deck{"ace of spade", showCard()}
 	//cards = append(cards, "ace of diamond")
 	//cards.print()
-	cards := newDeck()
-	hand, remainingCards := deal(cards, 3)
-	hand.print()
-	remainingCards.print()
+	//cards := newDeck()
+	//hand, remainingCards := deal(cards, 3)
+	//hand.print()
+	//remainingCards.print()
+	//cards.saveToFile("my_deck")
+	cards := newDeckFromFile("my_deck")
+	fmt.Println(cards)
 }
 
 //func showCard() string{
