b451736ce1b1d5f12ca0029631aac56d6f88bbee
Ben Lu <hilltop16@gmail.com>
2018-01-11 14:16:19 +0800

    added newDeck function to start a new deck of cards

--- deck.go
+++ deck.go
@@ -4,6 +4,24 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	// create a deck slice deck := []string{}
+	cards := deck{}
+	// create suits slice
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	// create value slice
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits{
+		for _, value := range cardValues{
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 
 func main() {
-	cards := deck{"ace of spade", showCard()}
-	cards = append(cards, "ace of diamond")
+	//cards := deck{"ace of spade", showCard()}
+	//cards = append(cards, "ace of diamond")
+	//cards.print()
+	cards := newDeck()
 	cards.print()
 }
 
-func showCard() string{
-	return "ace of heart"
-}
+//func showCard() string{
+//	return "ace of heart"
+//}
